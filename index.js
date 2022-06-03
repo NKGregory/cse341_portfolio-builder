aa6bacf379db7129743626d5b49c7a7f284f0f7c
NKGregory <b01194921@byui.edu>
2022-06-02 19:09:35 -0600

    set up auth0

--- index.js
+++ index.js
@@ -7,6 +7,38 @@ const connect = require ('./db/connect');
 const swaggerUI = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
+
+
+
+
+
+const { auth, requiresAuth } = require('express-openid-connect');
+require('dotenv').config();
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+
+app.get('/profile',requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
+
+
 connect.initDatatbase();
 
 app
@@ -18,7 +50,8 @@ app
       res.setHeader('Access-Control-Allow-Origin', '*');
       next()
   });
-  
+
+
 process.on('uncaughtException', (err, origin) => {
   console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
 });
